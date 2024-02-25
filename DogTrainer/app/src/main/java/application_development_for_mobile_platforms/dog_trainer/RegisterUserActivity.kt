2d1a4f29c25728eec1d4e4f5a9faccccbc2c32a3
Anatoliy Denezhniy <a.denezhnyy@crpo.su>
2024-02-25 17:13:51 +0300

    refs #3: rename main activity to register user activity and set listener to sing in button

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/RegisterUserActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/RegisterUserActivity.kt
@@ -5,31 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 
-class AuthUserActivity : AppCompatActivity() {
+class RegisterUserActivity : AppCompatActivity() {
     private lateinit var signInButton: AppCompatButton
-    private lateinit var signUpButton: AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth_user)
+        setContentView(R.layout.activity_register_user)
 
         addListenerToSignInButton()
-        addListenerToSignUpButton()
     }
 
     private fun addListenerToSignInButton() {
         signInButton = findViewById(R.id.singInButton)
         signInButton.setOnClickListener {
-            val intent = Intent(this@AuthUserActivity, ViewUserMeActivity ::class.java)
-            startActivity(intent)
-            finish()
-        }
-    }
-
-    private fun addListenerToSignUpButton() {
-        signUpButton = findViewById(R.id.signUpButton)
-        signUpButton.setOnClickListener {
-            val intent = Intent(this@AuthUserActivity, RegisterUserActivity ::class.java)
+            val intent = Intent(this@RegisterUserActivity, AuthUserActivity ::class.java)
             startActivity(intent)
             finish()
         }

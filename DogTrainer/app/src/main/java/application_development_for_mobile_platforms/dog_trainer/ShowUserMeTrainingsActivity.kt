abb8dd0e8f1d9d2971e5e6cf1e1e3f9f7d1f6fde
Anatoliy Denezhniy <a.denezhnyy@crpo.su>
2024-02-28 21:29:50 +0300

    refs #4: created template of show user me trainings screen

---
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ShowUserMeTrainingsActivity.kt
@@ -0,0 +1,11 @@
+package application_development_for_mobile_platforms.dog_trainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowUserMeTrainingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_user_me_trainings)
+    }
+}

5b95441fa87ad4e7e1bcd178bc1bd24db6a37337
Kostebelova-Elizaveta <lizbet227@gmail.com>
2024-03-02 21:12:47 +0300

    Add form to add training

---
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/FormToAddTrainingActivity.kt
@@ -0,0 +1,11 @@
+package application_development_for_mobile_platforms.dog_trainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormToAddTrainingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_to_add_training)
+    }
+}

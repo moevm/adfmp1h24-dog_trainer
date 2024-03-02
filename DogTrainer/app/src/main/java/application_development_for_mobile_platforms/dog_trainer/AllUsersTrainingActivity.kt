e032e190880da1a6b000abe9c8b8014fe2aa0ec3
Kostebelova-Elizaveta <lizbet227@gmail.com>
2024-03-02 17:05:11 +0300

    Add all users training page

---
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/AllUsersTrainingActivity.kt
@@ -0,0 +1,11 @@
+package application_development_for_mobile_platforms.dog_trainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllUsersTrainingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_users_training)
+    }
+}

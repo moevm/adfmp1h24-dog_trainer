ec0f082b9feeda05b96ef47da570219026c1057a
Anatoliy Denezhniy <a.denezhnyy@crpo.su>
2024-03-02 21:07:20 +0300

    refs : added toolbar to user achievements screen

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/UserAchievementsActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/UserAchievementsActivity.kt
@@ -1,11 +1,12 @@
 package application_development_for_mobile_platforms.dog_trainer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class UserAchievementsActivity : AppCompatActivity() {
+class UserAchievementsActivity : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_achievements)
+
+        initToolbar()
     }
 }
--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/UserMeAchievementsActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/UserMeAchievementsActivity.kt
@@ -3,9 +3,11 @@ package application_development_for_mobile_platforms.dog_trainer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class UserMeAchievementsActivity : AppCompatActivity() {
+class UserMeAchievementsActivity : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_me_achievements)
+
+        initToolbar()
     }
 }

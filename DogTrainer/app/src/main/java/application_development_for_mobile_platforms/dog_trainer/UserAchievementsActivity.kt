ce149e8d5de13ab15c0bf253cbbe67ac4e6d25bd
Kostebelova-Elizaveta <lizbet227@gmail.com>
2024-03-02 20:41:41 +0300

    Create User achievements page (guest)

---
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/UserAchievementsActivity.kt
@@ -0,0 +1,11 @@
+package application_development_for_mobile_platforms.dog_trainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserAchievementsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_achievements)
+    }
+}

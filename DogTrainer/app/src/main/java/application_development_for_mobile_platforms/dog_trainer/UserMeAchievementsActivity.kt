e3c298a59f04eaae954f4689e5ef7d18355ce009
Kostebelova-Elizaveta <lizbet227@gmail.com>
2024-03-02 20:38:40 +0300

    rename files

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/UserMeAchievementsActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/UserMeAchievementsActivity.kt
@@ -3,9 +3,9 @@ package application_development_for_mobile_platforms.dog_trainer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class UserAchievementsActivity : AppCompatActivity() {
+class UserMeAchievementsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_achievements)
+        setContentView(R.layout.activity_user_me_achievements)
     }
 }

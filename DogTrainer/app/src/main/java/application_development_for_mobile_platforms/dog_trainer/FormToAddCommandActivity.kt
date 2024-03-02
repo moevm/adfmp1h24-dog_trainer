709a72c7f1e512af1d709d8053b9b03e7d9013b5
Kostebelova-Elizaveta <lizbet227@gmail.com>
2024-03-02 19:35:34 +0300

    Form to add commmand

---
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/FormToAddCommandActivity.kt
@@ -0,0 +1,11 @@
+package application_development_for_mobile_platforms.dog_trainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormToAddCommandActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_to_add_command)
+    }
+}

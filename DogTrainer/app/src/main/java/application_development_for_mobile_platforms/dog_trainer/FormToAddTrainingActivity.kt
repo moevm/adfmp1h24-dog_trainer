fdf3366648963fcf07e58d98a99141cf351f8a5b
Anatoliy Denezhniy <a.denezhnyy@crpo.su>
2024-03-02 21:29:13 +0300

    refs : added toolbar to add training form screen

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/FormToAddTrainingActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/FormToAddTrainingActivity.kt
@@ -3,9 +3,11 @@ package application_development_for_mobile_platforms.dog_trainer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class FormToAddTrainingActivity : AppCompatActivity() {
+class FormToAddTrainingActivity : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_to_add_training)
+
+        initToolbar()
     }
 }

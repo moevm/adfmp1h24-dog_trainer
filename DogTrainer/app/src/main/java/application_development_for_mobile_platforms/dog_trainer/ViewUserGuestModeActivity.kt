62b8352d93d910f5a7780f1bdb71075e26c186f7
Kostebelova-Elizaveta <lizbet227@gmail.com>
2024-03-02 22:36:09 +0300

    add view user profile in guest mode

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewUserGuestModeActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewUserGuestModeActivity.kt
@@ -1,11 +1,12 @@
 package application_development_for_mobile_platforms.dog_trainer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ViewUserGuestModeActivity : AppCompatActivity() {
+class ViewUserGuestModeActivity : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_user_guest_mode)
+
+        initToolbar()
     }
 }

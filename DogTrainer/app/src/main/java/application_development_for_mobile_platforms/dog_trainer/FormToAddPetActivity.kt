152329024ae7f54cd3875c3f2f4501f797e6e090
Kostebelova-Elizaveta <lizbet227@gmail.com>
2024-03-02 18:45:32 +0300

    Add form to add pet

---
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/FormToAddPetActivity.kt
@@ -0,0 +1,13 @@
+package application_development_for_mobile_platforms.dog_trainer
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+class FormToAddPetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_to_add_pet)
+    }
+}

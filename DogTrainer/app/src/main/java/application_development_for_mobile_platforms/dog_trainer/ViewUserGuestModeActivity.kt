fe9bf7dc4ca30f4db3838ea10a89e02ffafcfef6
Kostebelova-Elizaveta <lizbet227@gmail.com>
2024-03-02 22:34:30 +0300

    create layout and add pic

---
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewUserGuestModeActivity.kt
@@ -0,0 +1,11 @@
+package application_development_for_mobile_platforms.dog_trainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewUserGuestModeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_user_guest_mode)
+    }
+}

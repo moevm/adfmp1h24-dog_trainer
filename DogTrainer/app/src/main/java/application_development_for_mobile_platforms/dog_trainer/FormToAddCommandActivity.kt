952a989b87fa3b78c26f874036490e1c0ca292af
Anatoliy Denezhniy <a.denezhnyy@crpo.su>
2024-03-03 18:57:56 +0300

    refs : added toolbar

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/FormToAddCommandActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/FormToAddCommandActivity.kt
@@ -1,11 +1,12 @@
 package application_development_for_mobile_platforms.dog_trainer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class FormToAddCommandActivity : AppCompatActivity() {
+class FormToAddCommandActivity : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_to_add_command)
+
+        initToolbar()
     }
 }

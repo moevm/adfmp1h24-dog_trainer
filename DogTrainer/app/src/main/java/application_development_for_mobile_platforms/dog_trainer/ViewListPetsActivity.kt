9fa02ae6577daf841c633aa708bad7d011bd31b4
Anatoliy Denezhniy <a.denezhnyy@crpo.su>
2024-03-03 19:56:51 +0300

    refs : link pet profile with pets list

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewListPetsActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewListPetsActivity.kt
@@ -1,12 +1,37 @@
 package application_development_for_mobile_platforms.dog_trainer
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class ViewListPetsActivity : ToolbarActivity() {
+    private lateinit var viewFirstPet: Button
+    private lateinit var viewSecondPet: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_list_pets)
 
         initToolbar()
+        addListenerViewFirstPetButton()
+        addListenerViewSecondPetButton()
+    }
+
+    private fun addListenerViewFirstPetButton() {
+        viewFirstPet = findViewById(R.id.viewFirstPet)
+        viewFirstPet.setOnClickListener {
+            val intent = Intent(this@ViewListPetsActivity, EditPetProfileActivity ::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun addListenerViewSecondPetButton() {
+        viewSecondPet = findViewById(R.id.viewSecondPet)
+        viewSecondPet.setOnClickListener {
+            val intent = Intent(this@ViewListPetsActivity, EditPetProfileActivity ::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }

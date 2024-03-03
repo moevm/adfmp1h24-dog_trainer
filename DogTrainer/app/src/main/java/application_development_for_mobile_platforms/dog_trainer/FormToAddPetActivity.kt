f2dcf17750ab180baaaf788384fff40fa932a8ca
Anatoliy Denezhniy <a.denezhnyy@crpo.su>
2024-03-03 18:49:36 +0300

    refs : added toolbar

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/FormToAddPetActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/FormToAddPetActivity.kt
@@ -1,13 +1,12 @@
 package application_development_for_mobile_platforms.dog_trainer
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import androidx.appcompat.app.AppCompatActivity
 
-class FormToAddPetActivity : AppCompatActivity() {
+class FormToAddPetActivity : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_to_add_pet)
+
+        initToolbar()
     }
 }

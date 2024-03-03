98c13a0cecb6ea8a132736f24b803b23436b3506
Ekaterina0303 <katya74246@mail.ru>
2024-03-03 16:41:47 +0300

    added toolbar pet profile for guest and edit profile

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewPetProfileActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewPetProfileActivity.kt
@@ -1,12 +1,11 @@
 package application_development_for_mobile_platforms.dog_trainer
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class ViewPetProfileGuestActivity : ToolbarActivity() {
+class ViewPetProfileActivity : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_pet_profile_guest)
+        setContentView(R.layout.activity_view_pet_profile)
 
         initToolbar()
     }

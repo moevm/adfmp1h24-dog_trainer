b1968b308f975d33b0abbdcd33537ee7e6d709ab
Ekaterina0303 <katya74246@mail.ru>
2024-03-03 16:26:34 +0300

    added toolbar pet profile not for guest

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewPetProfile.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewPetProfile.kt
@@ -6,15 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ViewPetprofile : AppCompatActivity() {
+class ViewPetProfile : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_view_petprofile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        initToolbar()
     }
 }

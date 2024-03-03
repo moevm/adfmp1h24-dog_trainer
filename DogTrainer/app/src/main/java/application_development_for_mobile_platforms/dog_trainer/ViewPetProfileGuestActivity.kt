766cebbcd46ff02416e376468cdd4bcfd406e982
Ekaterina0303 <katya74246@mail.ru>
2024-03-03 16:00:39 +0300

    added view pet profile

---
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewPetProfileGuestActivity.kt
@@ -0,0 +1,11 @@
+package application_development_for_mobile_platforms.dog_trainer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ViewPetProfileGuestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pet_profile_guest)
+    }
+}

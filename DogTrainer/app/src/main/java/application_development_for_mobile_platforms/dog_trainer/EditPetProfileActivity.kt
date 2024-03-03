0ea8245eab46b4cb52ac289637e141f38ae782db
Ekaterina0303 <katya74246@mail.ru>
2024-03-03 16:00:52 +0300

    added edit pet profile

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/EditPetProfileActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/EditPetProfileActivity.kt
@@ -1,14 +1,11 @@
 package application_development_for_mobile_platforms.dog_trainer
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class ViewPetProfileActivity : AppCompatActivity() {
+class EditPetProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_pet_profile)
+        setContentView(R.layout.activity_edit_pet_profile)
     }
 }

a7567d8aee8affa1e0e3ed9d4e2016ce5f7de378
Ekaterina0303 <katya74246@mail.ru>
2024-03-03 16:09:34 +0300

    added view pet profile not for guest

---
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewPetprofile.kt
@@ -0,0 +1,20 @@
+package application_development_for_mobile_platforms.dog_trainer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ViewPetprofile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_view_petprofile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

fe228b109f2ec0a2a41525ffef7827cf2fbd2841
Anatoliy Denezhniy <a.denezhnyy@crpo.su>
2024-03-03 19:28:11 +0300

    refs : added redirect to command screen when click on command image

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/CatalogCommandActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/CatalogCommandActivity.kt
@@ -1,15 +1,20 @@
 package application_development_for_mobile_platforms.dog_trainer
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.view.View
 
 class CatalogCommandActivity : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalog_command)
+
         initToolbar()
+    }
+
+    fun onImageClick(view: View) {
+        val intent = Intent(this@CatalogCommandActivity, CommandActivity ::class.java)
+        startActivity(intent)
+        finish()
     }
 }

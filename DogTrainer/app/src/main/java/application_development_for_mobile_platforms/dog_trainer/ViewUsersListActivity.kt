9b1b9a5de7ead63341003bca6ad3cebcc5eeddcc
Anatoliy Denezhniy <a.denezhnyy@crpo.su>
2024-03-04 22:26:40 +0300

    refs : made users list screen

--- DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewUsersListActivity.kt
+++ DogTrainer/app/src/main/java/application_development_for_mobile_platforms/dog_trainer/ViewUsersListActivity.kt
@@ -1,12 +1,37 @@
 package application_development_for_mobile_platforms.dog_trainer
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class ViewUsersListActivity : ToolbarActivity() {
+    private lateinit var viewUserButtonFirst: Button
+    private lateinit var viewUserButtonSecond: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_users_list)
 
         initToolbar()
+        addListenerToViewUserButtonFirst()
+        addListenerToViewUserButtonSecond()
+    }
+
+    private fun addListenerToViewUserButtonFirst() {
+        viewUserButtonFirst = findViewById(R.id.button1)
+        viewUserButtonFirst.setOnClickListener {
+            val intent = Intent(this@ViewUsersListActivity, ViewUserGuestModeActivity ::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun addListenerToViewUserButtonSecond() {
+        viewUserButtonSecond = findViewById(R.id.button2)
+        viewUserButtonSecond.setOnClickListener {
+            val intent = Intent(this@ViewUsersListActivity, ViewUserGuestModeActivity ::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
